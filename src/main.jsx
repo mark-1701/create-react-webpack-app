10547e481055503833eab20e10744b872197921f
mark-1701 <vini.muralles@gmail.com>
2024-12-27 19:21:36 -0600

    add (main): strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'; // Asegúrate de importar desde 'react-dom/client'
+import { StrictMode } from 'react';
 import App from './components/App';
 import './style.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
